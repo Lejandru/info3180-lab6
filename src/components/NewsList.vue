15dff216e330c1fb3aabb5520101cb405f27ebf1
Lejandru Richards <Lejandru13@gmail.com>
2022-04-02 18:13:24 -0500

    Search Update

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,33 +1,44 @@
 <style scoped>
 .style{
-  color: ;
+  
 }
 </style>
 
 <template>
     <ul class="news__list">
-       <li v-for="article in articles"
-class="news__item">
+       <li v-for="article in articles" class="news__item">
+    <div class="style"> 
+        <img v-bind:src="article.urlToImage" /> <h4>{{ article.title }} </h4> {{ article.description }}
+    </div> 
 
-<div class="style"> <img v-bind:src="article.urlToImage" /> <h4>{{ article.title }} </h4> {{ article.description }}  </div> 
-
- </li>
-
+</li>
+<form  @submit.prevent="searchNews" class="d-flex flex-column justify-content-center">
+    <div class="input-group mx-sm-3 mb-2">
+        <label class="visually-hidden" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        <button class="btn btn-primary mb-2">Search</button>
+    </div>
+    <p>You are searching for {{ searchTerm }}</p>
+</form>
     </ul>
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            articles: []
+            articles: [],
+            searchTerm: ''
         };
     },
 
-    created() {
-        let self = this;
-        fetch('https://newsapi.org/v2/top-headlines?country=us',
-{
+    methods: {
+        searchNews() {
+            let self = this;
+
+            fetch('https://newsapi.org/v2/everything?q='+
+self.searchTerm + '&language=en', {
     headers: {
         Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
     }
@@ -40,10 +51,9 @@ export default {
             self.articles = data.articles;
         });
     }
+}
 
-
-
-};
+}
 </script>
 
 
